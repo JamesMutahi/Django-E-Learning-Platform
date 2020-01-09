f3045767f7f244214c3167143b6ef84eb19096b5
JamesMutahi <mutahijames0@gmail.com>
2020-01-09 12:48:25 +0300

    Add content and module management

--- courses/views.py
+++ courses/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMix
 from django.forms.models import modelform_factory
 from django.apps import apps
 
-from .models import Module, Content
+from .models import Module, Content, Course
 
 from django.shortcuts import redirect, get_object_or_404
 from django.views.generic.base import TemplateResponseMixin, View
@@ -159,3 +159,11 @@ class ContentDeleteView(View):
         content.item.delete()
         content.delete()
         return redirect('module_content_list', module.id)
+
+
+class ModuleContentListView(TemplateResponseMixin, View):
+    template_name = 'courses/manage/module/content_list.html'
+
+    def get(self, request, module_id):
+        module = get_object_or_404(Module, id=module_id, course__owner=request.user)
+        return self.render_to_response({'module': module})
